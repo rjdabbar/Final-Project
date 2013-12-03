c185e3214494ce3aae9d0ae474e2875f03357165
RJ Dabbar <rjdabbar@gmail.com>
2013-12-03 15:44:50 -0500

    updated token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SteamApp::Application.config.secret_key_base = '8ddd782d67e0c95cf799eb04626c3171c92ed4fc0824e24dd722ac31e85aa55cf3dc3bfbb7a9ae250cb4ab5c6234966d1a2b1158ba5cb8f585c24a59f1bd8caa'
+SteamApp::Application.config.secret_key_base = ENV['SECRET_TOKE']
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SteamApp::Application.routes.draw do
   get "welcome/index"
   root 'welcome#index'
+  post 'auth/steam/callback' => 'welcome#auth_callback'
 
 end
