499a0ac48dbd296fa555d0dd88f55fae6b5d46eb
RJ Dabbar <rjdabbar@gmail.com>
2013-12-03 16:35:05 -0500

    Parsing works

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,12 @@ class WelcomeController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :auth_callback
 
   def index
+    @player_summary = []
+    if session.key? :current_user
+      url = URI.parse("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=#{ENV['STEAM_WEB_API_KEY']}&steamids=#{session[:current_user][:uid]}")
+      res = Net::HTTP::get(url)
+      @player_summary = JSON.parse(res)
+    end
   end
 
 
