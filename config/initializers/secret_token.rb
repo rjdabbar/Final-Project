5ad4eaebb77afeff1cd80f66c298499c9ac9c034
RJ Dabbar <rjdabbar@gmail.com>
2013-12-03 15:45:30 -0500

    updated secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SteamApp::Application.config.secret_key_base = ENV['SECRET_TOKE']
+SteamApp::Application.config.secret_key_base = ENV['SECRET_TOKEN']
